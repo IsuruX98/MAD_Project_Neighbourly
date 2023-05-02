8661d64d2f14a8dbed398883ef36daadbe72ddf0
IT21169380 <isurusanka98@gmail.com>
2023-05-03 03:49:12 +0530

    main donation fund created

---
+++ app/src/main/java/com/mad/neighbourlytest/models/isuru/MainFundModel.kt
@@ -0,0 +1,8 @@
+package com.mad.neighbourlytest.models.isuru
+
+data class MainFundModel(
+    val name:String?=null,
+    val email:String?=null,
+    val comment:String?=null,
+    val amount:String?=null
+)

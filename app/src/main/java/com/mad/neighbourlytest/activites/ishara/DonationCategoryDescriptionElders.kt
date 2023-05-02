76f78acf776f0d955bdd76bd402043296dd0b36b
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-05-02 22:14:08 +0530

    aded userwise folders

---
+++ app/src/main/java/com/mad/neighbourlytest/activites/ishara/DonationCategoryDescriptionElders.kt
@@ -0,0 +1,22 @@
+package com.mad.neighbourlytest.activites.ishara
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mad.neighbourlytest.activites.isuru.DonateActivity
+import com.mad.neighbourlytest.databinding.ActivityDonationCategoryDescriptionEldersBinding
+
+class DonationCategoryDescriptionElders : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDonationCategoryDescriptionEldersBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDonationCategoryDescriptionEldersBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.orphanBtnDonateNow.setOnClickListener {
+            startActivity(Intent(this, DonateActivity::class.java))
+        }
+    }
+}
---
+++ app/src/main/java/com/mad/neighbourlytest/activites/ishara/ItemDonationActivity.kt
@@ -0,0 +1,12 @@
+package com.mad.neighbourlytest.activites.ishara
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mad.neighbourlytest.R
+
+class ItemDonationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_donation_request)
+    }
+}

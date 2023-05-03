4581cf6960bf54adecae21c374f0737488607dcf
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-05-04 03:10:36 +0530

    changed whitespaces

--- app/src/main/java/com/mad/neighbourlytest/adapters/ItemAdapter.kt
+++ app/src/main/java/com/mad/neighbourlytest/adapters/ItemAdapter.kt
@@ -46,9 +46,6 @@ class ItemAdapter (private val itemDonateList : ArrayList<ItemDonationModel>) : 
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val donationID : TextView = itemView.findViewById(R.id.DonationID)
-
-
-
         val typeDonation : TextView = itemView.findViewById(R.id.inputTypeDonation)
         val qtyDonation : TextView = itemView.findViewById(R.id.inputQty)
         val expDonation : TextView = itemView.findViewById(R.id.inputExp)

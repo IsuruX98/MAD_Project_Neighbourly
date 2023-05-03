1838bc84fd7aba56b3f0f6a57de7a4605790d235
IT21186042 <it21186042@my.sliit.lk>
2023-05-03 17:41:10 +0530

    model class for model

---
+++ app/src/main/java/com/mad/neighbourlytest/models/ishara/FamilyModel.kt
@@ -0,0 +1,13 @@
+package com.mad.neighbourlytest.models.ishara
+
+class FamilyModel (
+
+
+    val familyID:String?=null,
+    val familyName:String?=null,
+    val noMembers:String?=null,
+    val familyAddress:String?=null,
+    val familyConNumber:String?=null,
+    val familyJob:String?=null
+
+    )

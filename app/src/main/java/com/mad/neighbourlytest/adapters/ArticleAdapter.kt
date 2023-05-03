7100e94bc130639639a4db9b78bbc3b3dd2ef3ec
yasirudeshan~ <rpyasiru@gmail.com>
2023-05-04 02:41:21 +0530

    fetched one card view part done

--- app/src/main/java/com/mad/neighbourlytest/adapters/ArticleAdapter.kt
+++ app/src/main/java/com/mad/neighbourlytest/adapters/ArticleAdapter.kt
@@ -5,27 +5,42 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.mad.neighbourlytest.R
-import com.mad.neighbourlytest.activites.yasiru.MyArticles
 import com.mad.neighbourlytest.models.yasiru.ArticleModel
 
 class ArticleAdapter (private val articles : ArrayList<ArticleModel>) : RecyclerView.Adapter<ArticleAdapter.ViewHolder>(){
+
+    private lateinit var myListner : OnItemClickListner
+
+    interface OnItemClickListner{
+        fun onItemClick(position: Int)
+    }
+
+    fun setOnItemClickListner(clickListner: OnItemClickListner){
+        myListner = clickListner
+    }
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-
         val articleView = LayoutInflater.from(parent.context).inflate(R.layout.activity_one_article, parent, false)
-
-        return ViewHolder(articleView)
-
+        return ViewHolder(articleView, myListner)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val currentArticle = articles[position]
         holder.subject.text = currentArticle.subject
+        holder.description.text = currentArticle.description
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(itemView: View, clickListner: OnItemClickListner) : RecyclerView.ViewHolder(itemView) {
         val subject : TextView = itemView.findViewById(R.id.articleViewSubject)
+        val description : TextView = itemView.findViewById(R.id.viewArticleDescription2)
+
+        init{
+            itemView.setOnClickListener{
+                clickListner.onItemClick(adapterPosition)
+            }
+        }
     }
 
     override fun getItemCount(): Int {

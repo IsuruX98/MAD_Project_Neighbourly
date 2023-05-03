5b6045c8c0d97af64aeac3dbf1bfcfe62106cc05
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-05-03 17:08:20 +0530

    added log cat and else part empty list

--- app/src/main/java/com/mad/neighbourlytest/activites/ishara/ItemDonationListFetch.kt
+++ app/src/main/java/com/mad/neighbourlytest/activites/ishara/ItemDonationListFetch.kt
@@ -1,8 +1,10 @@
 package com.mad.neighbourlytest.activites.ishara
 
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
@@ -57,6 +59,8 @@ class ItemDonationListFetch : AppCompatActivity() {
 
                     itemRecyclerView.visibility = View.VISIBLE
 
+                }else{
+                    Log.d("my-tag","mukuth na hehe")
                 }
             }
 

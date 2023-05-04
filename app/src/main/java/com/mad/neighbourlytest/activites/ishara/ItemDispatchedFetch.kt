c8098b1010e8f41b48b69a967bb3d6e994f5c172
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-05-04 14:38:20 +0530

    dispatch item fetch done successfully

---
+++ app/src/main/java/com/mad/neighbourlytest/activites/ishara/ItemDispatchedFetch.kt
@@ -0,0 +1,86 @@
+package com.mad.neighbourlytest.activites.ishara
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.mad.neighbourlytest.R
+import com.mad.neighbourlytest.adapters.ItemAdapter
+import com.mad.neighbourlytest.models.ItemDonationModel
+
+class ItemDispatchedFetch: AppCompatActivity() {
+
+    private lateinit var itemRecyclerView: RecyclerView
+    private lateinit var itemList : ArrayList<ItemDonationModel>
+    private lateinit var database : DatabaseReference
+    private lateinit var showEmpty : TextView
+    private lateinit var dispatchBtn : Button
+    private lateinit var delBtn : Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_dispatched_fetch)
+
+
+        itemRecyclerView = findViewById(R.id.itemRecycler)
+        itemRecyclerView.layoutManager = LinearLayoutManager(this)
+        itemRecyclerView.setHasFixedSize(true)
+        showEmpty = findViewById(R.id.emptyList)
+        dispatchBtn = findViewById(R.id.dispatchedBtnDonation)
+        delBtn = findViewById(R.id.deleteBtn)
+
+
+        itemList = arrayListOf<ItemDonationModel>()
+
+        getDonationItemData()
+    }
+
+    private fun getDonationItemData(){
+        itemRecyclerView.visibility = View.GONE
+
+        database = FirebaseDatabase.getInstance().getReference("Donation Items")
+
+        database.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                itemList.clear()
+                if(snapshot.exists()){
+                    for(items in snapshot.children){
+
+                        val itemData = items.getValue(ItemDonationModel::class.java)
+                        if (itemData != null) {
+                            if(itemData.dispatched){
+                                itemList.add(itemData!!)
+                            }
+                        }
+
+                    }
+
+                    val mAdapter = ItemAdapter(itemList)
+                    itemRecyclerView.adapter = mAdapter
+
+                    itemRecyclerView.visibility = View.VISIBLE
+
+
+                }else{
+                    showEmpty.visibility = View.VISIBLE
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+    }
+}
